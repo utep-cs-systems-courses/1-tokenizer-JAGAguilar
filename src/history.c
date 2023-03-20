20c0ff4a9c1d6a82d2c1676599d0df6180a44440
Antonio <joseaguilarbusiness@gmail.com>
2023-03-19 22:09:10 -0600

    didn't push before

--- src/history.c
+++ src/history.c
@@ -3,41 +3,58 @@
 #include "history.h"
 
 List* init_history(){
-  List *hist = NULL;
+  List *hist = (List*)malloc(sizeof(List));
+  return hist;
   
 };
 
 void add_history(List *list, char *str){
-  if(list==NULL){
-    list.root.str = str;
-    list.root.id = 0;
+  if(*list->root == 0){
+    Item *item = (Item*)malloc(sizeof(Item));
+    item->id = 0;
+    char *start = token_start(str);
+    item->str = copy_str(start,token_terminator(str)-start);
+    list->root = item;
+    return;
   }
   else{
-    int count =0;
-    while(list!=NULL){
-      list++;
-      count++;
+    Item *temp = list->root;
+    while(temp->next !=0){
+      temp = temp->next;
     }
-    list.root.str = str;
-    list.root.id = count;
+    Item *item = (Item*)malloc(sizeof(Item));
+    item->id = temp->id+1;
+    char *start = token_start(str);
+    item->str = copy_str(start,token_terminator(str)-start);
+    temp->next = item;
   }
 };
 
 char *get_history(List *list,int id){
-  while(list.root.id!=id){
-    list++;
+  if(list == 0){
+    return 0;
   }
-  return list.root.str;
+  if(list->root->id == id){
+    return list->root->str;
+  }
+  Item *temp = list->root;
+  while(temp != 0 && temp->id != id){
+    temp = temp->next;
+  }
+  if(temp == 0){
+    return (char*) "Unavailable: No History";
+  }
+  return temp ->str;
 };
 
 void print_history(List *list){
-  while(list!=NULL){
-    printf("%d",list.root.str);
-    list++;
+  Item *temp = list->root;
+  while(temp != 0){
+    printf("%d. %s\n",temp->id,temp->str);
+    temp = temp->next;
   }
 }
 
 void free_history(List *list){
-  //rework file to work with tokenizer
-
+  free(list);
 };
